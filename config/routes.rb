18db20acd83fe6c339cbb7eb75eb15522fad1821
mtrimner <matt.trimner@gmail.com>
2020-10-29 12:58:35 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'static#home'
+  resources :rides
+  resources :attractions
+  resources :users
+  get '/signin', to: 'sessions#new', as: 'signin'
+  resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
